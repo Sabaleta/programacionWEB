a04003a6f1d65ae0551bfa1825f2d80bf9a97c05
lucas <lyuquisabe@gmail.com>
2025-03-13 20:54:51 -0300

    first commit

---
+++ python/test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import numpy as np
+
+# Create the numpy array
+g = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+
+# Check the type of g
+print(type(g))  # <class 'numpy.ndarray'>
+
+# Check the shape of g
+print(g.shape)  # (2, 3)
+
+# Check the data type of g
+print(g.dtype)  # dtype('int32')
